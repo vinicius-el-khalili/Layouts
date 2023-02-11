d1cb3f565ebdd5e3f60768b0782c65910629215d
vinicius <velkhaliliborsato@gmail.com>
2023-02-10 21:54:58 -0300

    multicolumns completed

--- pages/Layouts/multicolumns/index.js
+++ pages/Layouts/multicolumns/index.js
@@ -1,21 +1,55 @@
 import style from "./multicolumns.module.css"
+import Image from "next/image";
 const multicolumns = () => {
     return (
-        <>
+        <div className={style.body}>
+            <header className={style.header}>
+                <h1>Page title</h1>
+            </header>
             <div className={style.gridContainer}>
-                <div className={style.pageTitle}><h1>Page title</h1></div>
-                <div className={style.mainPicture}></div>
-                <div className={style.mainText}>
-                    <h2>Main Text</h2>
-                    <p>lorem ipstesium stuff lorem ipstesium stuff lorem ipstesium stuff lorem ipstesium stuff lorem ipstesium stuff lorem ipstesium stuff lorem ipstesium stuff </p>
-                </div>
-                <div className={style.gallery}>
-                    <div className={style.smallPicture}></div>
-                    <div className={style.smallPicture}></div>
-                    <div className={style.smallPicture}></div>    
-                </div>
+                <article className={style.mainArticle}>
+                    <div className={style.mainPicture}>
+                        <Image src={"/img/img1.jpg"} fill={true}></Image>
+                    </div>
+                    <div className={style.mainText}>
+                        <h2>Title</h2>                        
+                        <p>Lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem</p>
+                    </div>
+                </article>
+
+                <article className={style.article}>
+                    <div className={style.smallPicture}>
+                        <Image src={"/img/img1.jpg"} fill={true}></Image>
+                    </div>
+                    <div className={style.smallText}>
+                        <h3>Title</h3>
+                        <p>Lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem</p>
+                    </div>
+                </article>
+
+                <article className={style.article}>
+                    <div className={style.smallPicture}>
+                        <Image src={"/img/img2.jpg"} fill={true}></Image>
+                    </div>
+                    <div className={style.smallText}>
+                        <h3>Title</h3>
+                        <p>Lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem</p>
+                    </div>
+                </article>
+
+                <article className={style.article}>
+                    <div className={style.smallPicture}>
+                        <Image src={"/img/img3.jpg"} fill={true}></Image>
+                    </div>
+                    <div className={style.smallText}>
+                        <h3>Title</h3>
+                        <p>Lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem ipsum epstemium stuff lorem</p>
+                    </div>
+                </article>
+
+
             </div>
-        </>
+        </div>
     );
 }
  
