74f99c47a36e73e90277a67219179b72b6678a92
vinicius <velkhaliliborsato@gmail.com>
2023-02-12 06:37:58 -0300

    fullwidth layout start

---
+++ pages/Layouts/fullwidth/index.js
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import style from "./fullwidth.module.css"
+import pic1 from "@/public/img/fw1.jpg"
+import pic2 from "@/public/img/fw2.jpg"
+
+const FullWidth = () => {
+    return ( <div className={style.body}>
+        <div className={style.gridContainer}>
+
+            <section className={style.mainSection}>
+                <nav>
+                    <p>Home</p>
+                    <p>Portfolio</p>
+                    <p>Contact</p>
+                </nav>
+                <section>
+                    <h1>Lorem</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum perspiciatis fuga eveniet voluptas illo eligendi voluptatum!</p>
+                </section>
+            </section>
+
+            <section className={style.pictureSection1}>
+                <Image src={pic1} fill style={{objectFit:"cover"}}></Image>
+            </section>
+            <section className={style.textSection1}>
+                <h2>About</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </section>
+
+            <section className={style.textSection2}>
+                <h2>About</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </section>
+            <section className={style.pictureSection2}>
+                <Image src={pic2} fill style={{objectFit:"cover"}}></Image>
+            </section>
+
+            
+            
+            
+
+            
+
+        </div>
+    </div>);
+}
+ 
+export default FullWidth;
