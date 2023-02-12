691e7214dce44a58a9223cb0fc72f4455e751751
vinicius <velkhaliliborsato@gmail.com>
2023-02-11 21:21:44 -0300

    masonry first try

--- pages/Layouts/masonry/index.js
+++ pages/Layouts/masonry/index.js
@@ -1,25 +1,53 @@
 import style from "./masonry.module.css"
+import Image from "next/image"
+import cherry1 from "@/public/img/cherry1.jpg"
+import cherry2 from "@/public/img/cherry2.jpg"
+import cherry3 from "@/public/img/cherry3.jpg"
+import cherry4 from "@/public/img/cherry4.jpg"
 const Masonry = () => {
-    return (
+    return (<>
         <div className={style.body}>
 
             <nav className={style.nav}>
                 
-                    <div className={style.pageTitle}>
-                        <h2>Cherry</h2>
-                        <h2>BLOSSOM</h2>
-                    </div>
+                <div className={style.pageTitle}>
+                    <h3>Cherry</h3>
+                    <h1>BLOSSOM</h1>
+                </div>
+                <div className={style.menu}>
                     <h3>Section</h3>
                     <h3>Section</h3>
                     <h3>Section</h3>
                     <h3>Section</h3>
+                </div>
 
             </nav>
+
+            <div className={style.gridContainer}>
+
+                <Image src={cherry1} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry2} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry3} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry4} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry4} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry3} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry2} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry1} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry2} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry4} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry1} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry3} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry3} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry1} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry4} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+                <Image src={cherry2} style={{display:"block",width:"100%",height:"auto"}} className={style.picture}></Image>
+
+            </div>
 
 
 
         </div>
-    );
+        </>);
 }
  
 export default Masonry;
